1c066a9c3f311737951f355df8600afbda97171e
Abdulrauf Kazi <kaziabdulrauf15@gmail.com>
2026-03-01 20:50:23 +0530

    fix:dark mode

--- app/components/ThemeProvider.tsx
+++ app/components/ThemeProvider.tsx
@@ -1,8 +1,16 @@
 "use client";
+
+import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
     return (
-        <NextThemesProvider attribute="class" defaultTheme="light" enableSystem={false} storageKey="ark-theme">
+        <NextThemesProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem={true}
+            disableTransitionOnChange
+        >
             {children}
         </NextThemesProvider>
     );
